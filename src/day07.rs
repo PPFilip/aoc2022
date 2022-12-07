4b869ce0e21ba25c6eaaf1f1b571ca9347b84124
PPFilip <7268500+PPFilip@users.noreply.github.com>
2022-12-07 12:09:18 +0100

    Day 07

---
+++ src/day07.rs
@@ -0,0 +1,158 @@
+#![allow(dead_code)]
+
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+
+
+pub fn day07part1(input_file: &str) -> u32 {
+    let file = File::open(input_file).unwrap();
+    let mut act_path: Vec<String> = Vec::new();
+
+    let mut file_sizes:HashMap<(Vec<String>, String),u32> = HashMap::new();
+    let mut dir_sizes:HashMap<Vec<String>, u32> = HashMap::new();
+
+    for l in io::BufReader::new(file).lines() {
+        let line = l.unwrap();
+        let v: Vec<&str> = line.split(' ').collect();
+        let pos0 = v.get(0).unwrap().to_owned();
+        let pos1 = v.get(1).unwrap().to_owned();
+        if pos0 == "$" {
+            if pos1 == "cd" {
+                let dir = v.get(2).unwrap().to_owned();
+                match dir {
+                    ".." => {
+                        act_path.pop(); ()
+                    },
+                    "/" => {
+                        act_path.clear()
+                    },
+                    _ => {
+                        act_path.push(dir.to_string())
+                    }
+                };
+
+                if !dir_sizes.contains_key(&*act_path.clone()) {
+                    dir_sizes.insert(act_path.clone(), 0);
+                }
+            }
+        } else {
+            if let Ok(size) = pos0.parse::<u32>() {
+                let fname = pos1.to_string();
+                if file_sizes.insert((act_path.clone(), fname), size) == None {
+
+                    let mut ap = act_path.clone();
+
+                    for _x in 0 .. ap.len()+1 {
+                        if let Some(act_size) = dir_sizes.get(&*ap.clone()) {
+                            dir_sizes.insert(ap.clone(), act_size+size);
+                            ap.pop();
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    let total = dir_sizes.values().filter(|&&x| x<=100000).sum();
+    println!("{total}");
+    total
+}
+
+pub fn day07part2(input_file: &str) -> u32 {
+    let file = File::open(input_file).unwrap();
+    let mut act_path: Vec<String> = Vec::new();
+
+    let mut file_sizes:HashMap<(Vec<String>, String),u32> = HashMap::new();
+    let mut dir_sizes:HashMap<Vec<String>, u32> = HashMap::new();
+
+    for l in io::BufReader::new(file).lines() {
+        let line = l.unwrap();
+        let v: Vec<&str> = line.split(' ').collect();
+        let pos0 = v.get(0).unwrap().to_owned();
+        let pos1 = v.get(1).unwrap().to_owned();
+        if pos0 == "$" {
+            if pos1 == "cd" {
+                let dir = v.get(2).unwrap().to_owned();
+                match dir {
+                    ".." => {
+                        act_path.pop(); ()
+                    },
+                    "/" => {
+                        act_path.clear()
+                    },
+                    _ => {
+                        act_path.push(dir.to_string())
+                    }
+                };
+
+                if !dir_sizes.contains_key(&*act_path.clone()) {
+                    dir_sizes.insert(act_path.clone(), 0);
+                }
+            }
+        } else {
+            if let Ok(size) = pos0.parse::<u32>() {
+                let fname = pos1.to_string();
+                if file_sizes.insert((act_path.clone(), fname), size) == None {
+
+                    let mut ap = act_path.clone();
+
+                    for _x in 0 .. ap.len()+1 {
+                        if let Some(act_size) = dir_sizes.get(&*ap.clone()) {
+                            dir_sizes.insert(ap.clone(), act_size+size);
+                            ap.pop();
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    const TOTAL_SIZE:u32 = 70000000;
+    const DESIRED_SIZE:u32 = 30000000;
+
+    let root_dir:Vec<String> = Vec::from([]);
+    let unused_space = TOTAL_SIZE - dir_sizes.get(&*root_dir).unwrap();
+    let find_free_space = DESIRED_SIZE - unused_space;
+
+    let mut total = u32::MAX;
+    for (_dir, size) in dir_sizes {
+        if find_free_space < size && size < total {
+            total = size;
+        }
+    }
+
+    println!("{total}");
+    total
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions;
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(day07part1("input/day07_example.txt"), 95437)
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(day07part1("input/day07.txt"), 1449447)
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(day07part2("input/day07_example.txt"), 24933642)
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(day07part2("input/day07.txt"), 8679207)
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
 
 use crate::day01::*;
 use crate::day02::*;
@@ -12,8 +13,9 @@ use crate::day03::*;
 use crate::day04::*;
 use crate::day05::*;
 use crate::day06::*;
+use crate::day07::*;
 
 
 fn main() {
-    day06part1();
+    day07part2("input/day07.txt");
 }
